a51b07dde56188297d3f138cc2d45fde7a1698b6
Ido <dev.xyzuniverse@gmail.com>
2025-09-16 11:59:37 +0200

    rodin-miuicamera: Move-out sepolicies into device tree
    Change-Id: Idb7e9878de477f91c3966839b489231c2647d8ff
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,5 @@ MIUICAMERA_PATH := device/xiaomi/rodin-miuicamera
 # Properties
 TARGET_SYSTEM_EXT_PROP += $(MIUICAMERA_PATH)/system_ext.prop
 
-# SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/private
-
 # Inherit from the proprietary version
 include vendor/xiaomi/rodin-miuicamera/BoardConfigVendor.mk
