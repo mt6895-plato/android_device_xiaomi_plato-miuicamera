cfa6c7dd7530de97ed3706e4e2393f33833b0667
Ido <dev.xyzuniverse@gmail.com>
2026-02-06 03:15:47 +0100

    Revert "rodin-miuicamera: Switch-out to bash extract script"
    This reverts commit 7a021baab0401738a45a7827bcf58059d5f268c9.
    
    Change-Id: If374924262586bd01db99067b2fb32c4b16bbc3d

---
+++ extract-files.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/rodin-miuicamera',
+    'vendor/xiaomi/rodin'
+]
+
+def lib_fixup_system_ext_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}-{partition}' if partition == 'system_ext' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    ('vendor.mediatek.hardware.camera.isphal@1.0',
+     'vendor.mediatek.hardware.camera.isphal-V1-ndk'): lib_fixup_system_ext_suffix
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
+        .apktool_patch('patches/'),
+
+    ('system_ext/lib64/libcamera_algoup_jni.xiaomi.so',
+     'system_ext/lib64/libcamera_mianode_jni.xiaomi.so',
+     'system_ext/lib64/libcamera_ispinterface_jni.xiaomi.so'): blob_fixup()
+        .add_needed('libgui_shim_miuicamera.so'),
+
+    'system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'rodin-miuicamera',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
